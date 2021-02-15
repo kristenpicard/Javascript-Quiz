393ea40ad914ab9a0f6d23a8d15c3524ba95f6ea
Kristen Picard <kristenpicard93@gmail.com>
2021-02-15 18:29:37 -0500

    Very start

---
+++ Assets/script.js
@@ -0,0 +1,62 @@
+var questions = [
+    {
+    question: "A very useful tool used during development and debugging for printing content to the debbugger is:",
+    answers: {
+        a: "JavaScript", 
+        b: "terminal/bash", 
+        c: "for loops", 
+        d: "console.log"
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "Commonly used data types DO NOT include",
+        answers: {
+            a: "strings", 
+            b: "booleans", 
+            c: "alerts", 
+            d: "numers"
+            },
+            correctAnswer: "c"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed within ______.",
+        answers: {
+            a: "quotes", 
+            b: "curly brackets", 
+            c: "parenthesis", 
+            d: "square brackets"
+            },
+            correctAnswer: "c"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store",
+        answers: {
+            a: "numbers and strings", 
+            b: "other arrays", 
+            c: "booleans", 
+            d: "all of the above"
+            },
+            correctAnswer: "d"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answers: {
+            a: "quotes", 
+            b: "curly brackets", 
+            c: "commas", 
+            d: "parenthesis"
+            },
+            correctAnswer: "a"
+    },
+    {
+        question: "Each piece of data contained in an array is called a(n)",
+        answers: {
+            a: "index", 
+            b: "element", 
+            c: "range", 
+            d: "column"
+            },
+            correctAnswer: "b"
+    },
+];

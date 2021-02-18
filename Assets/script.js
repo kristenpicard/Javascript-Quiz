ba1f18436faf30c5f5cdd937f34090dff92bf13a
Kristen Picard <kristenpicard93@gmail.com>
2021-02-17 20:04:40 -0500

    Progress

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,12 @@
 var questions = [
     {
-    question: "A very useful tool used during development and debugging for printing content to the debbugger is:",
-    answers: {
-        a: "JavaScript", 
-        b: "terminal/bash", 
-        c: "for loops", 
-        d: "console.log"
-        },
+        question: "A very useful tool used during development and debugging for printing content to the debbugger is:",
+        answers: {
+            a: "JavaScript", 
+            b: "terminal/bash", 
+            c: "for loops", 
+            d: "console.log"
+            },
         correctAnswer: "d"
     },
     {
@@ -17,7 +17,7 @@ var questions = [
             c: "alerts", 
             d: "numers"
             },
-            correctAnswer: "c"
+        correctAnswer: "c"
     },
     {
         question: "The condition in an if / else statement is enclosed within ______.",
@@ -27,7 +27,7 @@ var questions = [
             c: "parenthesis", 
             d: "square brackets"
             },
-            correctAnswer: "c"
+        correctAnswer: "c"
     },
     {
         question: "Arrays in JavaScript can be used to store",
@@ -37,7 +37,7 @@ var questions = [
             c: "booleans", 
             d: "all of the above"
             },
-            correctAnswer: "d"
+        correctAnswer: "d"
     },
     {
         question: "String values must be enclosed within _____ when being assigned to variables.",
@@ -47,7 +47,7 @@ var questions = [
             c: "commas", 
             d: "parenthesis"
             },
-            correctAnswer: "a"
+        correctAnswer: "a"
     },
     {
         question: "Each piece of data contained in an array is called a(n)",
@@ -57,22 +57,50 @@ var questions = [
             c: "range", 
             d: "column"
             },
-            correctAnswer: "b"
+        correctAnswer: "b"
     },
 ];
 
-function checkAnswer () {
 
-};
-function nextQuestion () {
 
-};
-function updateTimer () {
 
+function checkAnswer (event) {
+    var element = event.target.id;
+    if (element == questions[questionCount].correctAnswer) {
+        questionCount++;
+        quizContainer.innerHTML = questions[questionCount].question; 
+        resultsContainer.textContent = "";
+        buttonA.textContent = questions[questionCount].answers.a;
+        buttonB.textContent = questions[questionCount].answers.b;
+        buttonC.textContent = questions[questionCount].answers.c;
+        buttonD.textContent = questions[questionCount].answers.d;
+    }
+    else {
+        resultsContainer.textContent = "Wrong!";
+    }
 };
-function updateScore () {
 
-};
-function updateScore () {
+var questionCount = 0;
 
-};
+
+var buttonA = document.querySelector("#a");
+var buttonB = document.querySelector("#b");
+var buttonC = document.querySelector("#c");
+var buttonD = document.querySelector("#d");
+var quizContainer = document.querySelector('#quiz');
+var resultsContainer = document.querySelector('#results');
+var buttonsContainer = document.querySelector('.buttons');
+
+// Default on first question.
+quizContainer.innerHTML = questions[0].question;
+buttonA.textContent = questions[0].answers.a;
+buttonB.textContent = questions[0].answers.b;
+buttonC.textContent = questions[0].answers.c;
+buttonD.textContent = questions[0].answers.d;
+
+buttonA.addEventListener("click", checkAnswer);
+buttonB.addEventListener("click", checkAnswer);
+buttonC.addEventListener("click", checkAnswer);
+buttonD.addEventListener("click", checkAnswer);
+
+
